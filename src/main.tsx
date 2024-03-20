9ccca4f94d89b745a81238ebb3b27b27a856d376
roodi <igorrud95@gmail.com>
2024-03-20 22:41:34 +0700

    6 sprint 1 lesson 1/2

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import { StrictMode } from 'react'
 
-import { App } from '@/App'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
+import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
-import './styles/index.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import { App } from './App'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
